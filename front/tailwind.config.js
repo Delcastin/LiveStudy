35a947aa7c51c5cd4b883c3097fcc984faf6767a
seojiho <happyluck0426@gmail.com>
2025-07-10 17:51:43 +0900

    chore(postcss): Tailwind v4 대응을 위한 설정 파일 수정

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        f9: '#F9F9F9',
+      },
+      borderColor: {
+        f9: '#F9F9F9',
+      },
+    },
   },
   plugins: [],
 };
