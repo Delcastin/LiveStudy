96d661d7366c7907e9a4623a757b96b2d3c0fb7b
JinwonShen <bosv031999@gmail.com>
2025-07-11 14:02:07 +0900

    style: 공통 스타일링 및 Tailwind 컬러 커스터마이징 적용

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: '#3674B5',
+          400: '#578FCA',
+        },
+        secondary: {
+          500: '#FADA7A',
+          400: '#F5F0CD',
+        },
+      },
+    },
   },
   plugins: [],
 };
