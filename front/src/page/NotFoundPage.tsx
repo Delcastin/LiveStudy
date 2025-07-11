88ebb0f2891aaa80a0596c9da5ac7630fbf52952
seojiho <happyluck0426@gmail.com>
2025-07-11 15:01:38 +0900

    style(notfound): NotFound 페이지 UI 구성 및 스타일 적용

--- front/src/page/NotFoundPage.tsx
+++ front/src/page/NotFoundPage.tsx
@@ -1,5 +1,15 @@
-function NotFoundPage() {
-  return <div>NotFoundPage</div>;
-}
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 p-6">
+      <h1 className="text-8xl md:text-9xl font-bold">404</h1>
+      <p className="text-xl md:text-2xl font-light mb-4">페이지를 찾을 수 없습니다.</p>
+      <Link to="/" className="medium-button bg-primary-500 text-white hover:bg-primary-400">
+        홈으로 돌아가기
+      </Link>
+    </div>
+  );
+};
 
 export default NotFoundPage;
